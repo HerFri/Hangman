e24bb1c96748bccbb43c04cedc6adb8f78569823
Her Fri <herbert.frichert@outlook.de>
2023-08-16 18:59:51 +0200

    add function start_game, providing basis for the game

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import random
 from wordlist import word_list
 
+
 def choose_word():
     """
     Function that chooses a random word from the wordlist
@@ -15,7 +16,8 @@ def init_game():
     """
     roundstart = input("Welcome to Hangman! Do you think you can rescue the poor man from hanging? y/n \n")
     if roundstart == "y":
-        print("Let's play!")
+        #print("Let's play!")
+        start_game()
     elif roundstart == "n":
         print("What a pitty! Maybe you will change your mind and come to play soon.")
     else:
@@ -24,10 +26,49 @@ def init_game():
         init_game()
 
 
-#def start_game():
-#    correct_guesses = []
-#    incorrect_guesses = []
-#    tries = 6
+def start_game():
+    """
+    Function for starting the actual game, including the previously defined funtions
+    """
+    word = random.choice(word_list)
+    tries = 6
+    correct_guesses = []
+    incorrect_guesses = []
+
+    #hangman_count = -1     #for future hangman display
+    
+
+    while tries > 0:
+        output = ""
+        for letter in word:
+            if letter in correct_guesses:
+                output += letter 
+            else:
+                output += "_ "
+        if output == word:
+            break 
+
+        print("Guess the word: ", output)
+        print(tries, " tries left")
+
+        guess = input().lower()
+        
+        if guess in correct_guesses or guess in incorrect_guesses:
+            print("Already guessed", guess)
+        elif guess in word:
+            print(f"Good job, the letter {guess} is in the word!")
+            correct_guesses.append(guess)
+        else:
+            print(f"Unfortunately, {guess} is not in the word. Try again!")
+            #hangman_count = hangman_count + 1
+            tries = tries - 1
+            incorrect_guesses.append(guess)
+            #print(f"Hangmancount: {hangman_count}")
+    if tries > 0:
+        print("Splendid! You guessed the word and you win! Congratulations, you saved a man's life!")
+    else:
+        print("Sorry you guessed the wrong letter. Try again!")
+
 
 
 
@@ -92,5 +133,7 @@ def main():
     choose_word()
 
 #main()
+
+#init_game()
 
 init_game()
