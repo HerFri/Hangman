0b14d0d158317cd8d5e9972b18c16cddb196a4f9
Her Fri <herbert.frichert@outlook.de>
2023-08-18 22:24:08 +0200

    edit print statement inside else condition

--- run.py
+++ run.py
@@ -28,7 +28,7 @@ def init_game():
     elif round_start == "n":
         print("What a pitty! Maybe you will change your mind and come back soon to save a man from hanging.")
     else:
-        print("Answer must be 'n','y' or 'r'")
+        print("Answer must be 'Y','N' or 'R'")
         init_game()
 
 def show_rules():
