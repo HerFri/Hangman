b13b2d7f29bd2e090197ed0e2f4385582d189673
Her Fri <herbert.frichert@outlook.de>
2023-08-18 14:24:45 +0200

    add functions win_round & lose_round, delete redundant and dead code

--- run.py
+++ run.py
@@ -10,16 +10,6 @@ title = HANGMANTITLE
 you_win = YOUWIN
 you_lose = YOULOSE
 
-
-#def choose_word():
-#    """
-#    Function that chooses a random word from the wordlist
-#    and returns it in uppercase for better readability
-#    """
-#   word = random.choice(word_list)
-#    return word.upper()
-
-
 def init_game():
     """
     Function that initializes game when user accepts to play
@@ -27,14 +17,32 @@ def init_game():
     print(title)
     roundstart = input("Welcome to Hangman! Do you think you can rescue the poor man from hanging? y/n \n").lower()
     if roundstart == "y":
-        #print("Let's play!")
         start_game()
     elif roundstart == "n":
         print("What a pitty! Maybe you will change your mind and come back soon to save a man from hanging.")
     else:
-        #raise ValueError("Answer must be 'n' or 'y'")
         print("Answer must be 'n' or 'y'")
         init_game()
+
+
+def win_round(word):
+    print(you_win)
+    print(f"Splendid! You guessed the right word {word} and saved the man from being hanged!")
+    nextround = input("Want to save some man's life again? y/n \n")
+    if nextround == "y":
+        start_game()
+    elif nextround == "n":
+        print("What a pitty! Maybe you will change your mind and come to play soon.")
+
+
+def lose_round(word):
+    print(you_lose)
+    print(f"Game Over! The word was {word}")
+    nextround = input("Want to play again? y/n \n")
+    if nextround == "y":
+        start_game()
+    elif nextround == "n":
+        print("What a pitty! Maybe you will change your mind and come back to save more men from hanging.")
 
 
 def start_game():
@@ -42,7 +50,9 @@ def start_game():
     Function for starting the actual game, when player agrees to play a game 
     """
     word = get_word()
+    
     tries = 6
+    
     correct_guesses = []
     incorrect_guesses = []
 
@@ -54,8 +64,6 @@ def start_game():
         for letter in word:
             if letter in correct_guesses:
                 output += letter
-            #elif letter = word:
-            #   print("You guessed the word right!")
             else:
                 output += "_ "
         if output == word:
@@ -69,27 +77,15 @@ def start_game():
             print(f"{guess} is not a letter!")
             continue
         #elif guess == word:
-            print(you_win)
-            print(f"Splendid! You guessed the right word {word} and saved the man from being hanged!")
-            nextround = input("Want to save some man's life again? y/n \n")
-            if nextround == "y":
-                start_game()
-            elif nextround == "n":
-                print("What a pitty! Maybe you will change your mind and come to play soon.")
-        #elif len(guess) > 1:
+        elif len(guess) > 1:
             print("Pick only one letter!")
             continue
 
         if guess in correct_guesses or guess in incorrect_guesses:
             print("Already guessed", guess)
         elif guess == word:
-            print(you_win)
-            print(f"Splendid! You guessed the right word {word} and saved the man from being hanged!")
-            nextround = input("Want to save some man's life again? y/n \n")
-            if nextround == "y":
-                start_game()
-            elif nextround == "n":
-                print("What a pitty! Maybe you will change your mind and come to play soon.")
+            win_round(word)
+
         elif guess in word:
             print(f"Good job, the letter {guess} is in the word!")
             correct_guesses.append(guess)
@@ -101,22 +97,9 @@ def start_game():
             incorrect_guesses.append(guess)
             print(hangman[hangman_counter])
     if tries > 0:
-        print(you_win)
-        print(f"Splendid! You guessed the right word {word} and saved the man from being hanged!")
-        nextround = input("Want to save some man's life again? y/n \n")
-        if nextround == "y":
-            start_game()
-        elif nextround == "n":
-            print("What a pitty! Maybe you will change your mind and come to play soon.")
-    
+        win_round(word)
     elif tries == 0:
-        print(you_lose)
-        print(f"Game Over! The word was {word}")
-        nextround = input("Want to play again? y/n \n")
-        if nextround == "y":
-            start_game()
-        elif nextround == "n":
-            print("What a pitty! Maybe you will change your mind and come back to save more men from hanging.")
+        lose_round(word)  
     else:
         print("Sorry you guessed the wrong letter. Try again!")
 
